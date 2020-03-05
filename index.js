a304fa2c72b72f4952020e33690889b8d0cf4b17
NT <nicolasturek@gmail.com>
2020-03-05 03:27:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,3 @@ function logShout(string) {
 function logWhisper(string) {
     console.log("hello");
 }
-
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === uppercase) {
-    return "I can\'t hear you!"
-  } else (string.toLowerCase() === lowercase){
-    return "YES INDEED!";
-  } else ( "I love you, Grandma." === true) {
-    return "I love you, too.";
-  }
